7b9d03ff61f140287a0cfeb3c31dbe1933983ae7
italofalves <italofalves@hotmail.com>
2023-12-18 02:02:49 -0300

    Programa para cadastro de funcionario parte 1

---
+++ CadastroDeFuncionario/CadastroDeFuncionario/Funcionario.cs
@@ -0,0 +1,35 @@
+﻿
+namespace CadastroDeFuncionario
+{
+     class Funcionario
+    {
+
+        public int Id { get; private set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Funcionario()
+        {
+        }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AumentoSalario(double salario)
+        {
+            Salario += Salario * salario / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Id.ToString() + ", "
+                + Nome
+                + ", Salario: "
+                + Salario.ToString("F2");
+        }
+    }
+}
---
+++ CadastroDeFuncionario/CadastroDeFuncionario/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CadastroDeFuncionario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite quantos funcionarios serao registrados: ");
+            int qt = int.Parse(Console.ReadLine());
+
+            List<Funcionario> funcionarios = new List<Funcionario>();
+
+           
+            for (int i = 0; i < qt; i++)
+            {
+                Console.Write("Digite o ID do funcionario: ");
+                int idFuncionario = int.Parse(Console.ReadLine());
+                Console.Write("Digite o nome do funcionario: ");
+                string nome = Console.ReadLine();
+                Console.Write("Digite o salario do funcionario: ");
+                double salario = double.Parse(Console.ReadLine());
+
+                funcionarios.Add(new Funcionario(idFuncionario, nome, salario));
+                Console.WriteLine();
+            }
+
+            foreach (Funcionario funcionario in funcionarios)
+            {
+                Console.WriteLine(funcionario);
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Entre com o ID do funcionario cujo vai acrescentar aumento: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Entre com a porcentagem de aumento: ");
+            double porc = double.Parse(Console.ReadLine());
+
+            funcionarios.Find(x => x.Id == id).AumentoSalario(porc);
+            Console.WriteLine();
+
+            foreach (Funcionario funcionario in funcionarios)
+            {
+                Console.WriteLine(funcionario);
+            }
+
+        }
+    }
+}
